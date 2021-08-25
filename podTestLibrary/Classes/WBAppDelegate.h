bce7e3377d972d29c02b6ae50649881617310656
webbwang <iosiwork@163.com>
2021-08-25 08:29:10 +0800

    no message

---
+++ podTestLibrary/Classes/WBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WBAppDelegate.h
+//  podTestLibrary
+//
+//  Created by webbwang on 08/24/2021.
+//  Copyright (c) 2021 webbwang. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
